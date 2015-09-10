501cce7a35d592624d6dae2e1502ef02d6f0a224
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-09-11 00:03:28 +0800

    Extract parse error message from Firefox

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-var isParseError = function (parsedDocument) {
+var getParseError = function (parsedDocument) {
     // http://stackoverflow.com/questions/11563554/how-do-i-detect-xml-parsing-errors-when-using-javascripts-domparser-in-a-cross
     var p = new DOMParser(),
         errorneousParse = p.parseFromString('<', 'text/xml'),
-        parsererrorNS = errorneousParse.getElementsByTagName("parsererror")[0].namespaceURI;
+        parsererrorNS = errorneousParse.getElementsByTagName("parsererror")[0].namespaceURI,
+        parseerrorMatches;
 
     if (parsererrorNS === 'http://www.w3.org/1999/xhtml') {
         // In PhantomJS the parseerror element doesn't seem to have a special namespace, so we are just guessing here :(
-        return parsedDocument.getElementsByTagName("parsererror").length > 0;
+        parseerrorMatches = parsedDocument.getElementsByTagName("parsererror");
+    } else {
+        parseerrorMatches = parsedDocument.getElementsByTagNameNS(parsererrorNS, 'parsererror');
     }
 
-    return parsedDocument.getElementsByTagNameNS(parsererrorNS, 'parsererror').length > 0;
+    if (parseerrorMatches.length > 0) {
+        return parseerrorMatches[0].textContent;
+    }
+
+    return undefined;
+};
+
+var extractParseError = function (content) {
+    var match = /(.+)\n/.exec(content);
+
+    if (match) {
+        return match[1];
+    }
+    return undefined;
 };
 
 var failOnParseError = function (doc) {
-    if (doc === null || isParseError(doc)) {
-        throw {
-            message: "Invalid source"
-        };
+    var errorMessage;
+
+    if (doc === null) {
+        throw new Error('Parse error');
+    }
+
+    var parseError = getParseError(doc);
+    if (parseError !== undefined) {
+        errorMessage = extractParseError(parseError) || 'Parse error';
+        throw new Error(errorMessage);
     }
 };
 
--- test/sane-domparser.js
+++ test/sane-domparser.js
@@ -3,6 +3,20 @@ var test = require('tape'),
 
 var saneDomParser = require('../index.js');
 
+var backupDOMParser = window.DOMParser;
+var mockDOMParser = function (returnValue) {
+    window.DOMParser = function () {
+        return {
+            parseFromString: function () {
+                return returnValue;
+            }
+        };
+    };
+};
+var restoreDOMParser = function () {
+    window.DOMParser = backupDOMParser;
+};
+
 test('should parse valid XML successfully', function (t) {
     var doc = saneDomParser.parseFromString('<xml></xml>', 'application/xml');
     t.equal(doc.documentElement.tagName, 'xml');
@@ -10,12 +24,29 @@ test('should parse valid XML successfully', function (t) {
 });
 
 test('should throw error on invalid input', function (t) {
+    var errorDOM = new DOMParser().parseFromString('<parsererror xmlns="http://www.mozilla.org/newlayout/xml/parsererror.xml"></parsererror>', 'text/xml');
+    mockDOMParser(errorDOM);
+
     t.throws(function () {
         saneDomParser.parseFromString('<xml', 'text/xml');
-    }, {message: 'Invalid source'});
+    }, /Parse error/);
 
+    restoreDOMParser();
     t.end();
 });
+
+test('should include error message from Firefox', function (t) {
+    var errorDOM = new DOMParser().parseFromString('<parsererror xmlns="http://www.mozilla.org/newlayout/xml/parsererror.xml">XML Parsing Error: prefix not bound to a namespace\n Location: file:////tmp/index.html\n Line Number 1, Column 57:<sourcetext>&lt;html xmlns="http://www.w3.org/1999/xhtml"&gt;&lt;head/&gt;&lt;body&gt;&lt;namespace:customtag&gt;&lt;!-- namespace:customtag--&gt;&lt;/namespace:customtag&gt;&lt;/body&gt;&lt;/html&gt;\n --------------------------------------------------------^</sourcetext></parsererror>', 'text/xml');
+    mockDOMParser(errorDOM);
+
+    t.throws(function () {
+        saneDomParser.parseFromString('some invalid XML', 'text/xml');
+    }, /XML Parsing Error: prefix not bound to a namespace/);
+
+    restoreDOMParser();
+    t.end();
+});
+
 
 if (navigator.userAgent.indexOf('PhantomJS') === -1) {
     test.createStream().pipe(report('out'));
